f4e3882c16cfd6901ca33e1a9dad0c2735cbdb43
thiyagudinesh <thiyagubca1996@gmail.com>
2019-05-16 13:15:12 +0530

    Abstract Commited

---
+++ src/org/cts/login/Java.java
@@ -0,0 +1,10 @@
+package org.cts.login;
+
+public abstract class Java {
+	public void javaCourse() {
+	}	public void oracleCourse()
+	{
+		System.out.println(" Oracle course started");
+	}
+
+}
---
+++ src/org/cts/login/Java_1.java
@@ -0,0 +1,14 @@
+package org.cts.login;
+
+public class Java_1 extends Java {
+public void javaCourse()
+{
+	System.out.println(" Java course Started");
+}
+public static void main(String[] args)
+{
+	Java_1 j=new Java_1();
+	j.javaCourse();
+	j.oracleCourse();
+}
+}
